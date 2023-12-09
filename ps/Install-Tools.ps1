cfd9bd7015454cfd4ae464e489077b95f3f7e8a6
burtnolej <burtnolej@googlemail.com>
2023-12-09 16:20:12 -0500

    minor mods mid commit

--- ps/Install-Tools.ps1
+++ ps/Install-Tools.ps1
@@ -1,8 +1,8 @@
 
-$source_folder="C:\Users\burtn\Downloads"
-$target_folder="C:\Users\burtn\Downloads\tmp"
-$lnk_folder="C:\Users\burtn\Downloads"
-$launch_file="foo2.txt"
+$source_folder="C:\Users\burtn\Tools"
+$target_folder="C:\Users\burtn\Tools\Deploy"
+$lnk_folder="C:\Users\burtn\Tools\Deploy"
+$launch_file="vbautils.xlsm"
 $icon_file="C:\Users\burtn\Downloads\veloxemblem_icon\velox.ico"
 
 # &"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe" .\96x96.png .\72x72.png .\64x64.png .\48x48.png .\36x36.png .\32x32.png .\24x24.png .\16x16.png velox.ico
@@ -16,11 +16,15 @@ if (Test-Path -Path $target_folder) {
 }
 
 
-$values = "foo.txt", "foo2.txt"
+$values = "excelvba\DV.xlsm\DV.xlsm","excelvba\vbautils.xlsm\vbautils.xlsm","excelvba\MV.xlsm\mV.xlsm","excelvba\MO.xlsm\MO.xlsm"
 
 for ($i=0; $i -lt $values.Length; $i++) {
 
-    $source_file = "$source_folder\" + $values[$i]
+    #$source_file = "$source_folder\" + $values[$i]
+
+    $source_file = Join-Path -Path $source_folder -ChildPath $values[$i]
+
+
     $target_file = "$target_folder\" + $values[$i]
 
     if (Test-Path -Path $source_file) {
@@ -39,6 +43,7 @@ for ($i=0; $i -lt $values.Length; $i++) {
     }
 }
 
+exit
 
 $lnk_file = "$lnk_folder\" + "velox.lnk"
 $shortcutFile = "$target_folder\" + $launch_file

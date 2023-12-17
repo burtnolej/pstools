1cd414a8058219b08be719cf68ac2e17a62d12ed
Jon Butler <burtnolej@goglemail.com>
2023-12-17 17:22:21 -0500

    added create exe call into script; commented out

--- ps/Install-ToolsExe.ps1
+++ ps/Install-ToolsExe.ps1
@@ -7,4 +7,8 @@ $deploy_tools_file = Join-Path -Path $working_dir -ChildPath "Deploy-Tools-Utils
 . $utils_file
 . $deploy_tools_file
 
+#create the exe
+#Invoke-PS2EXE .\Unpackup-Tools-Nodep.ps1 .\Unpackup-Tools-Nodep.exe
+
+
 Deploy-ToolsExe
